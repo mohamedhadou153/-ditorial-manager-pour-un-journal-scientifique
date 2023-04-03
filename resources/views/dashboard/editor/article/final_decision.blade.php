b6b98ee361b79cea7fe8364ba63764c74ae83393
khalid-eddriouch <khalid.tan7@gmail.com>
2023-04-03 00:19:40 +0000

    modified:   resources/views/dashboard/editor/article/final_decision.blade.php

--- resources/views/dashboard/editor/article/final_decision.blade.php
+++ resources/views/dashboard/editor/article/final_decision.blade.php
@@ -1,6 +1,12 @@
 @extends('dashboard.editor.header')
 @section('style')
 @vite('resources/css/app.css')
+<?php use Illuminate\Support\Facades\DB; use Illuminate\Support\Facades\Auth;   $articles = DB::table('articles')->select('*')
+        ->where('etat','!=','traitement')
+        ->where('editorId',auth::guard('editor')->user()->email)
+        ->where('reviewer1Id','!=', null)
+        ->where('reviewer2Id','!=', null)
+        ->get();?>
 <h2>bonjour</h2>
 
 @endsection
