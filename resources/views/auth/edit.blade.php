02824813a519ac741ca2124e60dbff5fc999056f
mohamedhadou157 <Hadoumohamed153@gmail.com>
2023-04-18 17:59:52 +0000

    kit

--- resources/views/auth/edit.blade.php
+++ resources/views/auth/edit.blade.php
@@ -1,17 +1,4 @@
-<head>
-<meta charset="utf-8">
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<title>brand article</title>
-<link href="https://fonts.googleapis.com/css?family=Merienda+One" rel="stylesheet">
-<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-<script src="https://kit.fontawesome.com/7f60b9a86b.js" crossorigin="anonymous"></script>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
 
 <style>
 	body {
@@ -138,10 +125,90 @@
 		}
 	}
 </style>
+<style>
+	img{
+		width: 100%;
+		display: block;
+	}
+	.container{
+		max-width: 1320px;
+		margin: 0 auto;
+		padding: 0 1.2rem;
+	}
+	header{
+		min-height: 100vh;
+		background: linear-gradient(rgba(0,0,0,0.4),rgba(0,0,0,0.4)), url('https://wallpaperaccess.com/full/19381.jpg') center/cover no-repeat fixed;
+		display: flex;
+		flex-direction: column;
+		justify-content: stretch;
+	}
+	.banner{
+		flex: 1;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		text-align: center;
+		color: #fff;
+		margin-bottom: 150px;
+	}
+	.banner-title{
+		font-size: 5rem;
+		line-height: 1.2;
+		font-family: 'Merienda One', sans-serif;
+	}
+	#a-buttom{
+		text-decoration: none; color:#F6F9FE
+	}
+	.button-17 {
+	align-items: center;
+	appearance: none;
+	background-color: transparent;
+	border-radius: 24px;
+	border-style: none;
+	box-shadow: rgba(0, 0, 0, .2) 0 3px 5px -1px,rgba(0, 0, 0, .14) 0 6px 10px 0,rgba(0, 0, 0, .12) 0 1px 18px 0;
+	box-sizing: border-box;
+	color: #3c4043;
+	cursor: pointer;
+	display: inline-flex;
+	fill: currentcolor;
+	font-family: "Google Sans",Roboto,Arial,sans-serif;
+	font-size: 14px;
+	font-weight: 1000;
+	height: 48px;
+	justify-content: center;
+	letter-spacing: .25px;
+	line-height: normal;
+	max-width: 100%;
+	overflow: visible;
+	padding: 2px 24px;
+	position: relative;
+	text-align: center;
+	text-transform: none;
+	transition: box-shadow 280ms cubic-bezier(.4, 0, .2, 1),opacity 15ms linear 30ms,transform 270ms cubic-bezier(0, 0, .2, 1) 0ms;
+	user-select: none;
+	-webkit-user-select: none;
+	touch-action: manipulation;
+	width: auto;
+	will-change: transform,opacity;
+	z-index: 0;
+	}
 
-@yield('style')
-</head> 
-<body>
+	.button-17:hover {
+	background: black;
+	color: #174ea6;
+	}
+
+	.button-17:active {
+	box-shadow: 0 4px 4px 0 rgb(60 64 67 / 30%), 0 8px 12px 6px rgb(60 64 67 / 15%);
+	outline: none;
+	}
+
+	.button-17:focus {
+	outline: none;
+	border: 2px solid #4285f4;
+	}
+</style>
+
 <header>
 	<nav class="navbar navbar-default">
 	<div class="navbar-header">
@@ -154,15 +221,24 @@
 			<span class="icon-bar"></span>
 		</button>
 	</div>
-	<div id="navbarCollapse" class="collapse navbar-collapse">
-				<div class="nav navbar-nav">
-				<li class="nav-item">
-                        <a class="nav-link"  href="{{ route('contact') }}">Contact</a>
-                    </li>
-                </div>
 	<!-- Collection of nav links, forms, and other content for toggling -->
 	<div id="navbarCollapse" class="collapse navbar-collapse">
-	
+		<!-- <ul class="nav navbar-nav">
+			<li class="active"><a href="/">Home</a></li>
+			<li class="active"><a href="/">Aboute</a></li>
+			<li class="dropdown">
+				<a data-toggle="dropdown" class="dropdown-toggle" href="#">Users<b class="caret"></b></a>
+				<ul class="dropdown-menu">					
+					<li><a href="{{route('author.create-article')}}">All Authors</a></li>
+					<li><a href="{{route('author.libre-article')}}">All Editors</a></li>
+					<li><a href="{{route('author.traitement-article')}}">All Reviewers</a></li>
+				</ul>
+			</li>
+			<li class="active"><a href="#article-section">Articles Section</a></li>
+
+		</ul> -->
+
+
 		<ul class="nav navbar-nav navbar-right">
 		   	
 			<li class="dropdown">
@@ -176,8 +252,13 @@
 		</ul>
 	</div>
 </nav>
-@yield('content')
-
+<div class="banner">
+	<div class="container">
+		<h1 class="banner-title">
+		    <span><i class="fa fa-cube"></i>Brand<b>Article</b></span> 
+			<p>Tout ce que vous devez savoir sur la science</p>
+			<!-- <button class="button-17" role="button" ><a id="a-buttom" href="#article-section" >Discover More</a></button> -->
+		</h1>
+	</div>
+</div>
 </header>
-</body>
-</html>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,90 @@
+
 @extends('layouts.header')
 @section('style')
+
+<style>
+	img{
+		width: 100%;
+		display: block;
+	}
+	.container{
+		max-width: 1320px;
+		margin: 0 auto;
+		padding: 0 1.2rem;
+	}
+	header{
+		min-height: 121vh;
+		background: linear-gradient(rgba(0,0,0,0.4),rgba(0,0,0,0.4)), url('https://wallpaperaccess.com/full/19381.jpg') center/cover no-repeat fixed;
+		display: flex;
+		flex-direction: column;
+		justify-content: stretch;
+	}
+	.banner{
+		flex: 1;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		text-align: center;
+		color: #fff;
+		margin-bottom: 150px;
+	}
+	.banner-title{
+		font-size: 5rem;
+		line-height: 1.2;
+		font-family: 'Merienda One', sans-serif;
+	}
+	#a-buttom{
+		text-decoration: none; color:#F6F9FE
+	}
+	.button-17 {
+	align-items: center;
+	appearance: none;
+	background-color: transparent;
+	border-radius: 24px;
+	border-style: none;
+	box-shadow: rgba(0, 0, 0, .2) 0 3px 5px -1px,rgba(0, 0, 0, .14) 0 6px 10px 0,rgba(0, 0, 0, .12) 0 1px 18px 0;
+	box-sizing: border-box;
+	color: #3c4043;
+	cursor: pointer;
+	display: inline-flex;
+	fill: currentcolor;
+	font-family: "Google Sans",Roboto,Arial,sans-serif;
+	font-size: 14px;
+	font-weight: 1000;
+	height: 48px;
+	justify-content: center;
+	letter-spacing: .25px;
+	line-height: normal;
+	max-width: 100%;
+	overflow: visible;
+	padding: 2px 24px;
+	position: relative;
+	text-align: center;
+	text-transform: none;
+	transition: box-shadow 280ms cubic-bezier(.4, 0, .2, 1),opacity 15ms linear 30ms,transform 270ms cubic-bezier(0, 0, .2, 1) 0ms;
+	user-select: none;
+	-webkit-user-select: none;
+	touch-action: manipulation;
+	width: auto;
+	will-change: transform,opacity;
+	z-index: 0;
+	}
+
+	.button-17:hover {
+	background: black;
+	color: #174ea6;
+	}
+
+	.button-17:active {
+	box-shadow: 0 4px 4px 0 rgb(60 64 67 / 30%), 0 8px 12px 6px rgb(60 64 67 / 15%);
+	outline: none;
+	}
+
+	.button-17:focus {
+	outline: none;
+	border: 2px solid #4285f4;
+	}
+</style>
 @endsection
 @section('content')
 <div class="banner">
@@ -14,8 +99,7 @@
 <div>
 	@include('layouts.footer')
   </div>
+   
 @endsection
 
-   
-</body>
-</html>
+  
