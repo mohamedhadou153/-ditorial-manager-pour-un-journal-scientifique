c4e8d2cd24120faae131848d0bcd5a28fe8a0db2
khalid-eddriouch <khalid.tan7@gmail.com>
2023-04-18 21:03:26 +0000

    modified:   resources/views/dashboard/admin/header.blade.php

--- resources/views/dashboard/admin/header.blade.php
+++ resources/views/dashboard/admin/header.blade.php
@@ -79,14 +79,14 @@
                 <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities"
                     aria-expanded="true" aria-controls="collapseUtilities">
                     <i class="fas fa-fw fa-wrench"></i>
-                    <span>nouvelles commandes</span>
+                    <span>nouvelles demandes</span>
                 </a>
                 <div id="collapseUtilities" class="collapse" aria-labelledby="headingUtilities"
                     data-parent="#accordionSidebar">
                     <div class="bg-white py-2 collapse-inner rounded">
                         <h6 class="collapse-header">Custom Utilities:</h6>
-                        <a class="collapse-item" href="{{route('admin.new_editors')}}">editeurs</a>
-                        <a class="collapse-item" href="{{route('admin.new_reviewers')}}">reviseurs</a>
+                        <a class="collapse-item" href="{{route('admin.new_editors')}}">éditeurs</a>
+                        <a class="collapse-item" href="{{route('admin.new_reviewers')}}">réviseurs</a>
                     </div>
                 </div>
             </li>
@@ -119,6 +119,13 @@
                     </div>
                 </div>
             </li>
+            <li class="nav-item">
+                <a class="nav-link collapsed" href="/">
+                <i class="fa-solid fa-address-book"></i>
+                    <span>Contacts</span>
+                </a>
+            </li>
+
 
 
 
